766a6fc95625b367e55c9f4835712225f035556a
Zeinab Mahfouz <Zeinab.H.Mahfouz@gmail.com>
2026-07-15 11:04:10 +0300

    update config.py and  move .env to the main directory

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,17 +1,9 @@
-import os
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-# تحديد مسار المجلد الرئيسي حيث يقع ملف .env بدقة
-base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-env_file_path = os.path.join(base_dir, ".env")
-
-class Settings(BaseSettings):
-    APP_NAME: str
-    APP_VERSION: str
-    OPENAI_API_KEY: str
-
-    # الطريقة الصحيحة والمعتمدة في Pydantic v2 لقراءة ملف الـ .env
-    model_config = SettingsConfigDict(env_file="/mnt/d/My projects/LLM-Projects/mini-rag-app/src/.env", extra="ignore")
-
+from pydantic_settings import BaseSettings,SettingsConfigDict
+class settings(BaseSettings):
+    APP_NAME:str
+    APP_VERSION:str
+    OPENAI_API_KEY:str
+    class Config:
+        env_file = ".env"
 def get_settings():
-    return Settings()
+    return settings()
