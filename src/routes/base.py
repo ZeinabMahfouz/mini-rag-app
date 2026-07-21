6131113ed0028c987657152a526da7a26c8bd29f
Zeinab Mahfouz <Zeinab.H.Mahfouz@gmail.com>
2026-07-21 12:57:09 +0300

    upload the files and make for them unique path ,make enum for constant ,make project controller

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
-from fastapi import APIRouter,Depends
-from src.helpers.config import get_settings,settings 
-
+from fastapi import FastAPI, APIRouter, Depends
 import os
+from src.helpers.config import get_settings, Settings
+
 base_router = APIRouter(
-    prefix="/api/v1",tags=["api_v1"],
+    prefix="/api/v1",
+    tags=["api_v1"],
 )
+
 @base_router.get("/")
-async def welcome(app_settings: settings = Depends(get_settings)):
-    
+async def welcome(app_settings: Settings = Depends(get_settings)):
+
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
-    return {"message": f"Welcome to {app_name}!", "version": app_version}
+
+    return {
+        "app_name": app_name,
+        "app_version": app_version,
+    }
